bf649d9fd6aedb1d94e357b7252221c913048a9f
Forrest Shi <huashi@cisco.com>
2014-11-20 23:25:59 +0800

    Create GetFileNameLenth.bat

---
+++ GetFileNameLenth.bat
@@ -0,0 +1,27 @@
+
+@echo off
+set InputFile=%~1
+set OutputFile=%~2
+set f=\
+set /a n=1
+
+:begin
+rem call set x=%%s:~-%n%,1%%
+call set x=%%InputFile:~-%n%,1%%
+::echo x is %x%
+::echo f is %f%
+::echo n is %n%
+
+if "%x%"=="%f%" goto :end
+set /a n+=1
+echo n is %n%
+goto :begin
+
+:end
+set /a n-=1
+echo n is %n%
+echo %n% >%OutputFile%
+pause
+
+goto :eof
+
